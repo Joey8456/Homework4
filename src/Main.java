1ef9ef3e8896a6c1fde0bf9b365699f99a13c821
Joey HOME PC <josephbarron1016@gmail.com>
2024-12-04 03:06:41 -0600

    up

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-/* Feed COMPLETED TESTED AND WORKING*/
+/* Feed */
 class Feed{
     private String _title;
     private String _desc;
@@ -14,7 +14,7 @@ class Feed{
     public String getDesc(){return _desc;}
     public String toString(){return _title + " " + _desc;}
 }
-/* NewsFeed COMPLETED TESTED AND WORKING*/
+/* NewsFeed */
 class NewsFeed {
     private ArrayList<Feed> _newsFeed;
     public NewsFeed() {
@@ -22,7 +22,7 @@ class NewsFeed {
     }
     public NewsFeed(String fileName) throws IOException {
         _newsFeed = new ArrayList<Feed>();
-        Scanner fileScanner = new Scanner(new File(fileName + ".txt"));
+        Scanner fileScanner = new Scanner(new File(fileName));
         String title;
         String description;
         try {
@@ -48,20 +48,15 @@ class NewsFeed {
 }
 
 /**********************************************************/
-/* Strategy Pattern Interface/Classes                     */
-
-// Provided: Strategy Interface 
+/* Strategy Pattern Interface/Classes  */
 interface AnalysisBehavior {
     double analyze(String[] words, String searchWord);
 }
-
-// Task: Complete Class CountIfAnalysis
 class CountIfAnalysis implements AnalysisBehavior {
    public double analyze(String[] words, String searchWord){
        boolean found = false;
        int i = 0;
        while (i < words.length && !found){
-           System.out.println(words[i]);
            if((words[i].replaceAll("\\p{Punct}","")).equalsIgnoreCase(searchWord)){
                found = true;
            }
@@ -71,41 +66,37 @@ class CountIfAnalysis implements AnalysisBehavior {
        else return -1;
     }
 }
-// Task: Complete Class CountAllAnalysis ADD DOCUMENTATION
 class CountAllAnalysis implements AnalysisBehavior {
     public double analyze(String[] words, String searchWord) {
         double cntr = 0;
-        for(int i = 0; i <words.length;i++){
-            if((words[i].replaceAll("\\p{Punct}","")).equalsIgnoreCase(searchWord)){
+        for (int i = 0; i < words.length; i++) {
+            if ((words[i].replaceAll("\\p{Punct}", "")).equalsIgnoreCase(searchWord)) {
                 cntr += 1;
             }
         }
         return cntr;
     }
 }
+/* The actual searching for the keywords is the same in both, the only difference is in CountALL I did an
+exhaustive search and in CountIf I did the opposite. The value returned by both also varies, CountAll returns a
+counter of the key word and CountIf returns a 1 if true and a -1 if false. A double is a good return type because
+doubles avoid any issues with further calculations or uses of that number returned avoiding the need to convert
+the number from integer to a double*/
 
-
-/**********************************************************/
 /* Observer Pattern Interface/Classes                     */
-
-
 interface Subject {  // Notifying about state changes 
     void subscribe(Observer obs);
     void unsubscribe(Observer obs);
     void notifyObservers(Feed f);
 }
-
 interface Observer {  // Waiting for notification of state changes 
     void update(Feed f, String platformName);
 }
-
-
 abstract class SocialMediaPlatform implements Subject {
     private String _name;
     private ArrayList<Feed> _feed;
     private ArrayList<Observer> _observers;
     private int _updateRate;
-
     public SocialMediaPlatform(String n, int x){
         _name = n;
         _feed = new ArrayList<Feed>();
@@ -123,11 +114,18 @@ abstract class SocialMediaPlatform implements Subject {
         for (Observer observer : _observers)
             observer.update(f, _name);
     }
-    public void generateFeed(NewsFeed nf){
-
+    public void generateFeed(NewsFeed nf) {
+        Random rand = new Random();
+        int randInt = rand.nextInt(100);
+        if (randInt < _updateRate) {
+            Feed f = nf.getRandomFeed();
+            _feed.add(f);
+            notifyObservers(f);
+        }
     }
-    public double analyzeFeed(String w, AnalysisBehavior ab){
-
+    public double analyzeFeed(String w, AnalysisBehavior ab) {
+        String[] s = toString().split(" ");
+        return ab.analyze(s, w);
     }
     public String toString(){
         String s = "";
@@ -136,14 +134,24 @@ abstract class SocialMediaPlatform implements Subject {
         return s;
     }
 }
-
-// Concrete Social Media Platforms 
+// Concrete Social Media Platforms
 class Instagram extends SocialMediaPlatform {
     public Instagram() {
         super("Instagram", 30);  // 30% update rate 
     }
+    /*I chose snapchat and tiktok because they are popular app with different expected update rates.Snapchat
+     does not have much of an algorithm so less frequent rate makes sense whereas TikTok is more alive and changing*/
 }
-
+class TikTok extends SocialMediaPlatform {
+    public TikTok(){
+        super("TikTok",30);
+    }
+}
+class SnapChat extends SocialMediaPlatform{
+    public SnapChat(){
+        super("SnapChat",10);
+    }
+}
 class User implements Observer{
     private String _name;
     private ArrayList<SocialMediaPlatform> _myfeeds;
@@ -152,7 +160,9 @@ class User implements Observer{
         _name = s;
         _myfeeds = new ArrayList<SocialMediaPlatform>();
     }
-    public void addPlatform(SocialMediaPlatform smp){_myfeeds.add(smp);}
+    public void addPlatform(SocialMediaPlatform smp){
+        _myfeeds.add(smp);
+    }
     public void update(Feed f, String s){
         for (int i=0; i<_myfeeds.size(); i++){
             SocialMediaPlatform smp = _myfeeds.get(i);
@@ -171,67 +181,86 @@ class User implements Observer{
         return s;
     }
 }
-
-/**********************************************************/
-/* Factory Pattern Interface/Classes                      */
-/**********************************************************/
-/* ADDED
+/* Factory Pattern Interface/Classes */
 // Factory Creator Interface 
 interface SMPFactory {
     SocialMediaPlatform createPlatform();
 }
-
 // Concrete Factory classes for each platform 
 class InstagramFactory implements SMPFactory {
     public SocialMediaPlatform createPlatform() {
         return new Instagram();
     }
 }
-
-*/ //ADDED
-public class Main {
-    public static void main(String[] args) throws IOException {
-
-
-
-
-        //Feed f = new Feed("orange Apple", "Orange. Orange Orange!@ Orange banna ola fgf");
-       // String[] s = f.toString().split(" ");
-       // CountAllAnalysis test = new CountAllAnalysis();
-        //double t = test.analyze(s,"Orange");
-       // System.out.println(t);
-        //NewsFeed news = new NewsFeed("hw4data");
-       // System.out.println(news.getRandomFeed());
-
+class TikTokFactory implements SMPFactory {
+    public SocialMediaPlatform createPlatform() {
+        return new TikTok();
+    }
+}
+class SnapChatFactory implements SMPFactory {
+    public SocialMediaPlatform createPlatform() {
+        return new SnapChat();
     }
 }
 
-
-        /*  // Create main newsfeed from file
-        NewsFeed nf = new NewsFeed("data.txt");
-
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Create main newsfeed from file
+        NewsFeed nf = new NewsFeed("hw4data.txt");
         // Create SMP factories 
         SMPFactory instagramFactory = new InstagramFactory();
-
+        SMPFactory tikTokFactory = new TikTokFactory();
+        SMPFactory snapChatFactory = new SnapChatFactory();
         // Create the platforms container and add SMPs 
         ArrayList<SocialMediaPlatform> platforms = new ArrayList<>();
         platforms.add(instagramFactory.createPlatform());
-
-        // Create Users and subscribe 
+        platforms.add(snapChatFactory.createPlatform());
+        platforms.add(tikTokFactory.createPlatform());
+        // Create Users and subscribe
         User user1 = new User("Alice");
-        user1.addPlatform(instagramFactory.createPlatform());
-        platforms.get(0).subscribe(user1); // notice 0 - not the best yet
-
-        // Run a simulation to generate random feeds for the SMPs 
-
-
+        User user2 = new User( "Joey");
+        User user3 = new User();
+        user1.addPlatform(instagramFactory.createPlatform());  //User1 subscribed to all
+        user1.addPlatform(tikTokFactory.createPlatform());
+        user1.addPlatform(snapChatFactory.createPlatform());
+        user2.addPlatform(tikTokFactory.createPlatform());    //User2 subscribed to two
+        user2.addPlatform(snapChatFactory.createPlatform());
+        user3.addPlatform(tikTokFactory.createPlatform());    //User3 subscribed to one
+        platforms.get(0).subscribe(user1);
+        platforms.get(1).subscribe(user1);
+        platforms.get(2).subscribe(user1);
+        platforms.get(1).subscribe(user2);
+        platforms.get(2).subscribe(user2);
+        platforms.get(2).subscribe(user3);
+        // Run a simulation to generate random feeds for the SMPs
+        System.out.println("Loading Feeds for 20 seconds stand by ...");
+        long startTime = System.currentTimeMillis();
+        long duration = 1;
+        while(System.currentTimeMillis() - startTime < duration){
+            for(int i =0; i<platforms.size();i++){
+                platforms.get(i).generateFeed(nf);
+            }
+        }
+        System.out.println("Finished Loading");
         // Perform analysis
-        AnalysisBehavior ab = new CountAllAnalysis();
-        System.out.println(platforms.get(0).analyzeFeed("guess", new CountAllAnalysis()));
-
+        AnalysisBehavior interesting = new CountAllAnalysis();
+        for (SocialMediaPlatform platform : platforms) {
+            double test = platform.analyzeFeed("new", interesting);
+            System.out.println((platform.getName()) + " has the word 'new' in it " + test + " times!");
+        }
+        AnalysisBehavior exists = new CountIfAnalysis();
+        for (SocialMediaPlatform platform : platforms) {
+            double test = platform.analyzeFeed("AI", exists);
+            if(test == 1) {
+                System.out.println((platform.getName()) + " does contain the word 'AI'");
+            }
+            else{
+                System.out.println((platform.getName()) + " does not contain the word 'AI'");
+            }
+        }
         // Print Users' Contents
-
+        System.out.println("User1 Contents\n" + user1.toString());
+        System.out.println("User2 Contents\n" + user2.toString());
+        System.out.println("User3 Contents\n" + user3.toString());
     }
-
-} 
-*/
+}
